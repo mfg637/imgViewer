65769b0fa9099fe7dbf8cca735ba3e9b7b81dd74
mfg637 <mfg636@gmail.com>
2018-11-09 22:10:54 +0200

    show image files in grid

--- gui/__init__.py
+++ gui/__init__.py
@@ -5,15 +5,10 @@ import PIL.Image
 from PIL import ImageTk
 import tkinter
 from tkinter import filedialog
-
-
-class GUI:
-    def __init__(self):
-        self.root = tkinter.Tk()
-        self._show_image_window = None
-
-    def show_image(self, img):
-        self._show_image_window = ShowImage(self.root, img)
+from . import ScrolledFrame
+import sys
+import os
+import filesystem
 
 
 class ShowImage:
@@ -24,3 +19,36 @@ class ShowImage:
         self._image = ImageTk.PhotoImage(scaled_img)
         self.image_label = tkinter.Label(self._root, image=self._image)
         self.image_label.pack()
+        self._root.attributes("-topmost", True)
+
+class GUI:
+    def __init__(self):
+        self.root = tkinter.Tk()
+        self._show_image_window = None
+        self.root.geometry("840x720")
+        self.thumbs_wrapper = ScrolledFrame.VerticalScrolledFrame(self.root, width=820, height=720)
+        self.thumbs_wrapper.pack()
+        self._img_icon = ImageTk.PhotoImage(PIL.Image.open(os.path.join(
+            os.path.dirname(sys.argv[0]),
+            "images",
+            "img_icon.png"
+        )))
+
+    def show_image(self, img):
+        self._show_image_window = ShowImage(self.root, img)
+
+    def open_dir(self, directory_path):
+        n = 4
+        i = 0
+        image_list = filesystem.get_image_files(directory_path)
+        for image_path in image_list:
+            item_wrapper = tkinter.Frame(self.thumbs_wrapper.interior)
+            tkinter.Label(item_wrapper, image=self._img_icon).pack(side="top")
+            tkinter.Label(
+                item_wrapper,
+                text=image_path.stem,
+                width=25,
+                wraplength=192
+            ).pack(side="top")
+            item_wrapper.grid(row=i//n, column=i%n, sticky=tkinter.N)
+            i += 1
--- imgview.py
+++ imgview.py
@@ -4,7 +4,10 @@
 import gui
 import decoders
 import sys
+import os
 
 gui_instance = gui.GUI()
-gui_instance.show_image(decoders.open_image(sys.argv[1]))
+gui_instance.open_dir(os.path.dirname(sys.argv[1]))
+if os.path.isfile(sys.argv[1]):
+    gui_instance.show_image(decoders.open_image(sys.argv[1]))
 gui_instance.root.mainloop()
