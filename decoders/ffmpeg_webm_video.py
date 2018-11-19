7ff316dbd51614e49971cf30fd1af220b881a6cf
mfg637 <mfg636@gmail.com>
2018-11-19 16:07:50 +0200

    WEBM: remove buffer

--- decoders/ffmpeg_webm_video.py
+++ decoders/ffmpeg_webm_video.py
@@ -12,12 +12,12 @@ class WebM_Video(ImageFile.ImageFile):
 
     format = "WEBM"
     format_description = "WebM video file"
+    _close_exclusive_fp_after_loading = False
 
     def _open(self):
         self._current_frame = 0
 
         data = ffmpeg.probe(self.filename)
-        print(data)
 
         if data['format']['format_name'] != "matroska,webm":
             raise SyntaxError("not a WEBM file")
@@ -28,7 +28,6 @@ class WebM_Video(ImageFile.ImageFile):
                 video = stream
         fps = eval(video['r_frame_rate'])
         self.info['duration'] = int(round(1 / fps * 1000))
-        print(video["width"], video["height"])
         self._size = (video["width"], video["height"])
 
         self.mode = "RGB"
@@ -51,9 +50,7 @@ class WebM_Video(ImageFile.ImageFile):
             self.info['loop'] = 0
             tmpbuf = self.process.stdout.read()
             self.raw_data_len = len(tmpbuf)
-            self.raw_data_buf = io.BytesIO(tmpbuf)
-            self.raw_data_buf.seek(0)
-            self.fp = io.BytesIO(self.raw_data_buf.read(self._frame_size))
+            self.fp = io.BytesIO(tmpbuf)
         else:
             self.info['loop'] = 1
             self.raw_data_len = self._frame_size
@@ -72,10 +69,8 @@ class WebM_Video(ImageFile.ImageFile):
         if frame >= self._n_frames or frame < 0:
             raise EOFError
 
-        self.raw_data_buf.seek(self._frame_size * frame)
-        self.fp = io.BytesIO(self.raw_data_buf.read(self._frame_size))
         self.tile = [
-            ("raw", (0, 0) + self.size, 0, (self.mode, 0, 1))
+            ("raw", (0, 0) + self.size, self._frame_size * frame, (self.mode, 0, 1))
         ]
         self.load()
 
@@ -83,6 +78,62 @@ class WebM_Video(ImageFile.ImageFile):
 
     def tell(self):
         return self._current_frame
+
+    def load(self):
+        "Same as ImageFile. Just remove self.fp = None command"
+
+        pixel = Image.Image.load(self)
+
+        if self.tile is None:
+            raise IOError("cannot load this image")
+        if not self.tile:
+            return pixel
+
+        readonly = 0
+
+        read = self.fp.read
+
+        seek = self.fp.seek
+
+        self.load_prepare()
+        err_code = -3  # initialize to unknown error
+
+        # sort tiles in file order
+        self.tile.sort(key=ImageFile._tilesort)
+
+        for decoder_name, extents, offset, args in self.tile:
+            decoder = Image._getdecoder(self.mode, decoder_name,
+                                        args, self.decoderconfig)
+            try:
+                seek(offset)
+                decoder.setimage(self.im, extents)
+                if decoder.pulls_fd:
+                    decoder.setfd(self.fp)
+                    status, err_code = decoder.decode(b"")
+                else:
+                    b = b""
+                    while True:
+                        s = read(self.decodermaxblock)
+
+                        b = b + s
+                        n, err_code = decoder.decode(b)
+                        if n < 0:
+                            break
+                        b = b[n:]
+            finally:
+                # Need to cleanup here to prevent leaks
+                decoder.cleanup()
+
+        self.tile = []
+        self.readonly = readonly
+
+        self.load_end()
+
+        if not ImageFile.LOAD_TRUNCATED_IMAGES and err_code < 0:
+            # still raised if decoder fails to return anything
+            ImageFile.raise_ioerror(err_code)
+
+        return Image.Image.load(self)
 
 
 Image.register_open(WebM_Video.format, WebM_Video, _accept)
