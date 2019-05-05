1096a6e3481b5507a440615853c272e06f3f1290
mfg637 <mfg636@gmail.com>
2019-05-05 19:57:23 +0300

    ffmpeg-webm-decoding: lazy loading animations frames, animations bugfix

--- decoders/ffmpeg_webm_video.py
+++ decoders/ffmpeg_webm_video.py
@@ -15,6 +15,8 @@ class WebM_Video(ImageFile.ImageFile):
     _close_exclusive_fp_after_loading = False
 
     def _open(self):
+        self._exclusive_fp = None
+        ImageFile._exclusive_fp = None
         self._current_frame = 0
 
         data = ffmpeg.probe(self.filename)
@@ -26,17 +28,19 @@ class WebM_Video(ImageFile.ImageFile):
         for stream in data['streams']:
             if stream['codec_type'] == "video":
                 video = stream
-        fps = eval(video['r_frame_rate'])
+        fps = eval(video['avg_frame_rate'])
         self.info['duration'] = int(round(1 / fps * 1000))
         self._size = (video["width"], video["height"])
         self.mode = "RGB"
         try:
             self.size = self._size
         except AttributeError:
+            size = self._size
             self.tile = [
                 ("raw", (0, 0) + self._size, 0, (self.mode, 0, 1))
             ]
         else:
+            size =  self.size
             self.tile = [
                 ("raw", (0, 0) + self.size, 0, (self.mode, 0, 1))
             ]
@@ -46,16 +50,20 @@ class WebM_Video(ImageFile.ImageFile):
                        '-f', 'image2pipe',
                        '-pix_fmt', 'rgb24',
                        '-an',
+                       '-r', str(fps),
                        '-vcodec', 'rawvideo', '-']
         self.process = subprocess.Popen(commandline, stdout=subprocess.PIPE)
-        self._frame_size = self._size[0]*self._size[1]*3
+        self._frame_size = size[0]*size[1]*3
+        self.loaded_frames = 0
         self.info['duration_of_video'] = float(data['format']['duration'])
+        print(self.info)
         if 30 >= self.info['duration_of_video']:
             self._exclusive_fp = None
             self.info['loop'] = 0
-            tmpbuf = self.process.stdout.read()
-            self.raw_data_len = len(tmpbuf)
-            self.fp = io.BytesIO(tmpbuf)
+            self.raw_data_len = self._frame_size*2
+            buffer = self.process.stdout.read(self.raw_data_len)
+            self.fp = io.BytesIO(buffer)
+            self.loaded_frames += 2
         else:
             self.info['loop'] = 1
             self.raw_data_len = self._frame_size
@@ -71,15 +79,33 @@ class WebM_Video(ImageFile.ImageFile):
         return self._n_frames > 1
 
     def seek(self, frame):
-        if frame >= self._n_frames or frame < 0:
+        #print("load frame", frame)
+        if self.process.poll() is not None and frame >= self._n_frames or frame < 0:
             raise EOFError
+        elif self.process.poll() is None:
+            #print("loaded frames", self.loaded_frames)
+            while (self.loaded_frames <= frame):
+                #print("loading frame")
+                buffer = self.process.stdout.read(self._frame_size)
+                #print(len(buffer))
+                if len(buffer)>0:
+                    self.fp.write(buffer)
+                    self.loaded_frames += 1
+                    self.raw_data_len += self._frame_size
+                    #print(self.loaded_frames)
+                else:
+                    self.process.terminate()
+                    raise EOFError
 
         self.tile = [
             ("raw", (0, 0) + self.size, self._frame_size * frame, (self.mode, 0, 1))
         ]
+        #print(self.raw_data_len)
+        #print(self.tile)
         self.load()
 
         self._current_frame = frame
+        #print("done loading")
 
     def tell(self):
         return self._current_frame
@@ -132,7 +158,6 @@ class WebM_Video(ImageFile.ImageFile):
         self.tile = []
         self.readonly = readonly
 
-        self.load_end()
 
         if not ImageFile.LOAD_TRUNCATED_IMAGES and err_code < 0:
             # still raised if decoder fails to return anything
